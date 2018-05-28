d9be40447b23eec23314d372b930a0ca63f4c681
dagrende <dag.rende@find-out.se>
2018-05-28 07:28:24 +0200

    read input bits in parallel through reg PIND

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ char encoderChange(char a, char b, char &prevSwitches);
 long int x, y, z;
 long outTime = 0;
 const long OUT_INTERVAL_MS = 1000;
+char prevBits;
 
 void setup() {
   Serial.begin(115200);
@@ -17,54 +18,45 @@ void setup() {
   pinMode(7, INPUT);
 
   x = y = z = 0;
-}
 
-char prevSwitches[3] = {-1, -1, -1};
+  prevBits = PIND;
+}
 
 void printCoords() {
   Serial.print(x);
   Serial.print(", ");
   Serial.print(y);
   Serial.print(", ");
-  Serial.print(z);
-  Serial.print("\n");
+  Serial.println(z);
 }
 
-void loop() {
-  x += encoderChange(digitalRead(2), digitalRead(3), prevSwitches[0]);
-  y += encoderChange(digitalRead(4), digitalRead(5), prevSwitches[1]);
-  z += encoderChange(digitalRead(6), digitalRead(7), prevSwitches[2]);
 
-  // print x, y, z each second
-  // long t = millis();
-  // if (t > outTime + OUT_INTERVAL_MS) {
-  //   outTime = t;
-  // }
+static char changeTable[4][4] = {
+ //00  01  10  11 - prevSwitches
+  { 0,  1, -1,  0}, //00 - switches
+  {-1,  0,  0,  1}, //01
+  { 1,  0,  0, -1}, //10
+  { 0, -1,  1,  0}  //11
+};
+
+
+void loop() {
+  char bits = PIND;
+  x -=  changeTable[(prevBits >> 2) & 3][(bits >> 2) & 3];
+  y -=  changeTable[(prevBits >> 4) & 3][(bits >> 4) & 3];
+  z -=  changeTable[(prevBits >> 6) & 3][(bits >> 6) & 3];
+  prevBits = bits;
 
   if (Serial.available() > 0) {
     printCoords();
     Serial.read();
   }
-}
 
-// call continously with the two phases A, B of the quadrature encoder
-// returns the change to the position based on any quadrature switch change
-char encoderChange(char a, char b, char &prevSwitches) {
-  // position change based on [fromSwitches][toSwitches]
-  // where switches is two bits AB
-  static char changeTable[4][4] = {
-   //00  01  10  11 - prevSwitches
-    { 0,  1, -1,  0}, //00 - switches
-    {-1,  0,  0,  1}, //01
-    { 1,  0,  0, -1}, //10
-    { 0, -1,  1,  0}  //11
-  };
 
-  char switches = (a << 1) + b;
-  if (prevSwitches == -1) {
-    prevSwitches = switches;
-  }
-  char change = changeTable[prevSwitches][switches];
-  prevSwitches = switches;
-  return change;
+  // // print x, y, z each second
+  // long t = millis();
+  // if (t > outTime + OUT_INTERVAL_MS) {
+  //   outTime = t;
+  //   printCoords();
+  // }
 }
