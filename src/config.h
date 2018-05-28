2553edcd37e3407b18ccfbb356f40f3b0444b2f1
dagrende <dag.rende@find-out.se>
2018-05-28 07:32:00 +0200

    changed from atmega328p to esp32

---
+++ src/config.h
@@ -0,0 +1,3 @@
+
+const char* ssid = "gnejs";
+const char* password = "badborste";
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,93 @@
+
 #include <Arduino.h>
+#include <WiFi.h>
+#include "config.h"
+#include "esp_task_wdt.h"
 
-char encoderChange(char a, char b, char &prevSwitches);
+const char xPhA = 25;
+const char xPhB = 26;
+const char yPhA = 12;
+const char yPhB = 13;
+const char zPhA = 14;
+const char zPhB = 15;
+#define get2bits(startBit, value) ((unsigned char)((value >> startBit) & 3))
+const volatile int* GPIOP = (volatile int *)GPIO_IN_REG;
 
-long int x, y, z;
-long outTime = 0;
-const long OUT_INTERVAL_MS = 1000;
-char prevBits;
+const static int changeTable[4][4] = {
+ //00  01  10  11 - prevSwitches
+  { 0,  1, -1,  0}, //00 - switches
+  {-1,  0,  0,  1}, //01
+  { 1,  0,  0, -1}, //10
+  { 0, -1,  1,  0}  //11
+};
+
+TaskHandle_t decoderTask;
+
+long x, y, z;
+
+esp_err_t show_esp_error(const char *title, esp_err_t result) {
+  if (result != ESP_OK) {
+    Serial.print("error ");
+    if (result == ESP_ERR_NOT_FOUND) {
+      Serial.println("ESP_ERR_NOT_FOUND");
+    } else if (result == ESP_ERR_INVALID_STATE) {
+      Serial.println("ESP_ERR_INVALID_STATE");
+    } else {
+      Serial.print(result);
+    }
+    Serial.print(" in: ");
+    Serial.print(title);
+  }
+  return result;
+}
+
+void decoderLoop(void *parameter) {
+  unsigned int prevBits = *GPIOP;
+  unsigned int bits;
+  int change;
+  int i;
+
+  while (1) {
+    for (i = 0; i < 1000000; i++) {
+      bits = *GPIOP;
+      x += changeTable[get2bits(xPhA, prevBits)][get2bits(xPhA, bits)];
+      y += changeTable[get2bits(yPhA, prevBits)][get2bits(yPhA, bits)];
+      z += changeTable[get2bits(zPhA, prevBits)][get2bits(zPhA, bits)];
+      prevBits = bits;
+    }
+    delay(1);
+  }
+}
 
 void setup() {
-  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);
+  btStop();
 
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
+  Serial.begin(115200);
+  while (!Serial) ;
+
+  pinMode(xPhA, INPUT);
+  pinMode(xPhB, INPUT);
+  pinMode(yPhA, INPUT);
+  pinMode(yPhB, INPUT);
+  pinMode(zPhA, INPUT);
+  pinMode(zPhB, INPUT);
+  pinMode(16, OUTPUT);
+
+  digitalWrite(16, 0);
 
   x = y = z = 0;
 
-  prevBits = PIND;
+  xTaskCreatePinnedToCore(
+    decoderLoop,    /* Task function. */
+    "decoderTask",  /* name of task. */
+    1000,           /* Stack size of task */
+    NULL,           /* parameter of the task */
+    1,              /* priority of the task */
+    &decoderTask,   /* Task handle to keep track of created task */
+    0);             /* Core */
+
+  Serial.println("ready!!");
 }
 
 void printCoords() {
@@ -30,33 +98,9 @@ void printCoords() {
   Serial.println(z);
 }
 
-
-static char changeTable[4][4] = {
- //00  01  10  11 - prevSwitches
-  { 0,  1, -1,  0}, //00 - switches
-  {-1,  0,  0,  1}, //01
-  { 1,  0,  0, -1}, //10
-  { 0, -1,  1,  0}  //11
-};
-
-
 void loop() {
-  char bits = PIND;
-  x -=  changeTable[(prevBits >> 2) & 3][(bits >> 2) & 3];
-  y -=  changeTable[(prevBits >> 4) & 3][(bits >> 4) & 3];
-  z -=  changeTable[(prevBits >> 6) & 3][(bits >> 6) & 3];
-  prevBits = bits;
-
   if (Serial.available() > 0) {
     printCoords();
     Serial.read();
   }
-
-
-  // // print x, y, z each second
-  // long t = millis();
-  // if (t > outTime + OUT_INTERVAL_MS) {
-  //   outTime = t;
-  //   printCoords();
-  // }
 }
