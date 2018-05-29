fd7f738add594688e3dd8e97cb0ef9fa937ab5b8
dagrende <dag.rende@find-out.se>
2018-05-29 20:45:38 +0200

    added point from angles calculation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <WiFi.h>
 #include "config.h"
 #include "esp_task_wdt.h"
+#include <math.h>
 
 const char xPhA = 25;
 const char xPhB = 26;
@@ -23,7 +24,9 @@ const static int changeTable[4][4] = {
 
 TaskHandle_t decoderTask;
 
-long x, y, z;
+long int u;   // angle between vertical pillar and arm b
+long int v;   // angle between arm b and arm c
+long int w;   // rotation around vertical pillar
 
 esp_err_t show_esp_error(const char *title, esp_err_t result) {
   if (result != ESP_OK) {
@@ -50,14 +53,16 @@ void decoderLoop(void *parameter) {
   while (1) {
     for (i = 0; i < 1000000; i++) {
       bits = *GPIOP;
-      x += changeTable[get2bits(xPhA, prevBits)][get2bits(xPhA, bits)];
-      y += changeTable[get2bits(yPhA, prevBits)][get2bits(yPhA, bits)];
-      z += changeTable[get2bits(zPhA, prevBits)][get2bits(zPhA, bits)];
+      w += changeTable[get2bits(xPhA, prevBits)][get2bits(xPhA, bits)];
+      v += changeTable[get2bits(yPhA, prevBits)][get2bits(yPhA, bits)];
+      u += changeTable[get2bits(zPhA, prevBits)][get2bits(zPhA, bits)];
       prevBits = bits;
     }
-    delay(1);
+    delay(1); // have to do this to keep the watchdog on the mat
   }
 }
+
+
 
 void setup() {
   WiFi.mode(WIFI_OFF);
@@ -74,9 +79,9 @@ void setup() {
   pinMode(zPhB, INPUT);
   pinMode(16, OUTPUT);
 
-  digitalWrite(16, 0);
+  digitalWrite(16, 0);  // 1 enable encoder inputs, 0 disable inputs and let esp32 pins float
 
-  x = y = z = 0;
+  w = v = u = 0;
 
   xTaskCreatePinnedToCore(
     decoderLoop,    /* Task function. */
@@ -90,12 +95,37 @@ void setup() {
   Serial.println("ready!!");
 }
 
+typedef struct {
+  float x, y, z;
+} Point3D;
+
+
+// measure
+const int revSteps = 120000;
+const int a = 200;
+const int b = 200;
+const int c = 200;
+const float uZero = 90;
+const float vZero = 90;
+const float wZero = 0;
+void calcXYZ(Point3D &p, long int uRaw, long int vRaw, long int wRaw) {
+  float u = uRaw * M_PI * 2 / revSteps;
+  float v = vRaw * M_PI * 2 / revSteps;
+  float w = wRaw * M_PI * 2 / revSteps;
+  p.y = (b * sin(u) - c * sin(u + v)) * cos(w);
+  p.z = b * cos(u) - c * cos(u + v) - a;
+  p.x = p.y * sin(w);
+}
+
 void printCoords() {
-  Serial.print(x);
+  Point3D p;
+  calcXYZ(p, u, v, w);
+
+  Serial.print(p.x);
   Serial.print(", ");
-  Serial.print(y);
+  Serial.print(p.y);
   Serial.print(", ");
-  Serial.println(z);
+  Serial.println(p.z);
 }
 
 void loop() {
